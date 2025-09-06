c88ea37249ca845515023b5cffb3a907c2278065
Alberto Almagro <alberalm@ucm.es>
2025-09-07 01:29:06 +0200

    Fix dependencies

--- keep_database_on.py
+++ keep_database_on.py
@@ -1,11 +1,10 @@
 from prefect import flow, task
 from prefect.logging import get_run_logger
-from prefect.deployments.steps.utility import pip_install_requirements
+from prefect_sqlalchemy import SqlAlchemyConnector
 
 
 @task
 def count_species():
-    from prefect_sqlalchemy import SqlAlchemyConnector
     database_block = SqlAlchemyConnector.load("biosenda-database")
     with database_block as engine:
         return len(engine.execute("""
@@ -17,6 +16,5 @@ def count_species():
 @flow
 def query_biosenda_database():
     logger = get_run_logger()
-    pip_install_requirements()
     n = count_species()
     logger.info(f"Species in database: {n}")
